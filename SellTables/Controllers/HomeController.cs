755ccb5d57999e07e8516f7a9405b58e2905f2dd
Fiatskovich Yauheniy <fiatskovich.w@gmail.com>
2016-11-05 12:20:27 +0300

    Locales

--- SellTables/Controllers/HomeController.cs
+++ SellTables/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultilingualSite.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,9 +7,37 @@ using System.Web.Mvc;
 
 namespace SellTables.Controllers
 {
+    [Culture]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        public ActionResult ChangeCulture(string lang)
+        {
+            string returnUrl = Request.UrlReferrer.AbsolutePath;
+            // Список культур
+            List<string> cultures = new List<string>() { "ru", "en"};
+            if (!cultures.Contains(lang))
+            {
+                lang = "ru";
+            }
+            // Сохраняем выбранную культуру в куки
+            HttpCookie cookie = Request.Cookies["lang"];
+            if (cookie != null)
+                cookie.Value = lang;   // если куки уже установлено, то обновляем значение
+            else
+            {
+
+                cookie = new HttpCookie("lang");
+                cookie.HttpOnly = false;
+                cookie.Value = lang;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return Redirect(returnUrl);
+        }
+    
+
+    public ActionResult Index()
         {
             return View();
         }
