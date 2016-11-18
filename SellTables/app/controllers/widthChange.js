78c3bbd7b39a7c0d6ea3120590644bccf6a7d39d
yaink <yaink@DESKTOP-9J0A41O>
2016-11-18 18:25:50 +0300

    askofoipejf

--- SellTables/app/controllers/widthChange.js
+++ SellTables/app/controllers/widthChange.js
@@ -1,37 +1,68 @@
 ﻿angular.module('widthChange', [])
   .controller('WidthChangeController', function ($scope, $http) {
-      var currentClasses = "col-md-11 col-sm-11 col-xs-10";
+      var currentClasses = "col-md-12 col-sm-12 col-xs-12";
+      var currentFontClass = "font100";
 
 
 
 
       $scope.changeTo40 = function () {
-          var element = angular.element(document.getElementById('content'));
+     
+          var element = angular.element(document.getElementsByName("content"));
+          console.log(element);
           element.removeClass(currentClasses);
-          currentClasses = "col-md-5 col-sm-5 col-xs-4 col-md-offset-4 col-sm-offset-4 col-xs-offset-4";
+          currentClasses = "col-md-6 col-sm-6 col-xs-6 col-md-offset-3 col-sm-offset-3 col-xs-offset-3";
           element.addClass(currentClasses);
    
       }
 
       $scope.changeTo60 = function () {
-          var element = angular.element(document.getElementById('content'));
+          var element = angular.element(document.getElementsByName('content'));
           element.removeClass(currentClasses);
-          currentClasses = "col-md-7 col-sm-7 col-xs-6 col-md-offset-3 col-sm-offset-3 col-xs-offset-3";
+          currentClasses = "col-md-8 col-sm-8 col-xs-8 col-md-offset-2 col-sm-offset-2 col-xs-offset-2";
           element.addClass(currentClasses);  
       }
 
       $scope.changeTo80 = function () {
-          var element = angular.element(document.getElementById('content'));
+          var element = angular.element(document.getElementsByName('content'));
           element.removeClass(currentClasses);
-          currentClasses = "col-md-9 col-sm-9 col-xs-8 col-md-offset-2 col-sm-offset-2 col-xs-offset-2";
+          currentClasses = "col-md-10 col-sm-10 col-xs-10 col-md-offset-1 col-sm-offset-1 col-xs-offset-1";
           element.addClass(currentClasses);   
       }
 
       $scope.changeTo100 = function () {
-          var element = angular.element(document.getElementById('content'));
+          var element = angular.element(document.getElementsByName('content'));
           element.removeClass(currentClasses);
-          currentClasses = "col-md-11 col-sm-11 col-xs-10";
+          currentClasses = "col-md-12 col-sm-12 col-xs-12";
           element.addClass(currentClasses);
+      }
+
+      $scope.changeFontTo70 = function () {
+          var element = angular.element(document.getElementsByName('content'));
+          element.removeClass(currentFontClass);
+          currentFontClass = "font70";
+          element.addClass(currentFontClass);
+      }
+
+      $scope.changeFontTo100 = function () {
+          var element = angular.element(document.getElementsByName('content'));
+          element.removeClass(currentFontClass);
+          currentFontClass = "font100";
+          element.addClass(currentFontClass);
+      }
+
+      $scope.changeFontTo200 = function () {
+          var element = angular.element(document.getElementsByName('content'));
+          element.removeClass(currentFontClass);
+          currentFontClass = "font200";
+          element.addClass(currentFontClass);
+      }
+
+      $scope.changeFontTo300 = function () {
+          var element = angular.element(document.getElementsByName('content'));
+          element.removeClass(currentFontClass);
+          currentFontClass = "font300";
+          element.addClass(currentFontClass);
       }
 
   });
