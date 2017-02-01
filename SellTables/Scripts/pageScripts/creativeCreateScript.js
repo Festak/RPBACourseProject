af19998fc74a0f7a748cc5f3171e8a905bf7d896
Евгений <@FESTAK>
2017-02-01 19:46:53 +0300

    done

--- SellTables/Scripts/pageScripts/creativeCreateScript.js
+++ SellTables/Scripts/pageScripts/creativeCreateScript.js
@@ -4,7 +4,10 @@
 
 document.getElementById("submit").onclick = function () { 
     document.getElementById("Chapter_Text").value = $('#summernote').summernote('code');
-    document.getElementById("Creative_Category_Name").value = $('ex1_value');
+    var element = document.getElementById("ex1_value");
+    console.log(element[0].value);
+ 
+    document.getElementById("Creative_Category_Name").value = $('ex1').val();
     document.getElementById("Chapter_TagsString").value = takeTags();
 };
 
